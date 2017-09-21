3b70b90b0fb28f29afff9d1a5717a57a0362b23a
noahkryz <nkryzano@ualberta.ca>
2017-09-20 19:25:24 -0600

    Added clear button functionality

--- app/src/main/java/ca/ualberta/cs/lonelytwitter/LonelyTwitterActivity.java
+++ app/src/main/java/ca/ualberta/cs/lonelytwitter/LonelyTwitterActivity.java
@@ -41,6 +41,7 @@ public class LonelyTwitterActivity extends Activity {
 
 		bodyText = (EditText) findViewById(R.id.body);
 		Button saveButton = (Button) findViewById(R.id.save);
+		Button clearButton = (Button) findViewById(R.id.clear);
 		oldTweetsList = (ListView) findViewById(R.id.oldTweetsList);
 
 		saveButton.setOnClickListener(new View.OnClickListener() {
@@ -73,6 +74,15 @@ public class LonelyTwitterActivity extends Activity {
 				//finish();
 
 			}
+		});
+		clearButton.setOnClickListener(new View.OnClickListener() {
+			public void onClick(View v) {
+				setResult(RESULT_OK);
+				tweets.clear();
+				saveInFile();
+				adapter.notifyDataSetChanged();
+			}
+
 		});
 	}
 
